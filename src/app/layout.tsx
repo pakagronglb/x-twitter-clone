8d10671b49ccc42f2457b768af7d489d2ba1502b
pakagronglb <pakagronglebel@gmail.com>
2024-12-28 13:33:19 +0700

    Audit favicon once again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ const dm_sans = DM_Sans({
 export const metadata: Metadata = {
   title: 'X Clone',
   description: 'X clone social media application -- but Y',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
